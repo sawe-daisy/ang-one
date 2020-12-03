d71332488a03651eeb88b7918e218cad21acd6cf
sawe-daisy <deesawe16@gmail.com>
2020-12-03 22:59:39 +0300

    app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HoodComponent } from './hood/hood.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoodComponent,
+    GameControlComponent,
+    EvenComponent,
+    OddComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hood/hood.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HoodComponent } from './hood.component';
+
+describe('HoodComponent', () => {
+  let component: HoodComponent;
+  let fixture: ComponentFixture<HoodComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HoodComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HoodComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
